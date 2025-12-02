086afc7006ef54383e599f3c847a6674b075e65c
Fernando45817 <al45817@un.edu.mx>
2025-12-02 10:24:17 -0600

    3er js

--- js.js
+++ js.js
@@ -2,6 +2,15 @@ function Nombre(){
 
     document.getElementById("nombre-usuario")
 
+    
+    var changedname = 
+
+    var nombre = document.getElementById('nombre-usuario').value;
+
+    changedname = nombre + a
+
+
+
 
 
     alert("Sus cambios se han guardado")
